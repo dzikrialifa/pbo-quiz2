c6249f0a6daa2e1e07327893ddc93618d76e8719
dzikri <dzikrialif99@gmail.com>
2018-11-25 08:31:38 +0700

    Class Informasi Barang

---
+++ src/BarangInfo.java
@@ -0,0 +1,12 @@
+/*
+*   DZIKRI ALIF AB
+*   Email : dzikrialif99@gmail.com
+*   Hawlet Packard
+*/
+public class BarangInfo {
+    private String Kopi,Susu,Gula;
+
+    public BarangInfo() {
+    }
+    
+}
