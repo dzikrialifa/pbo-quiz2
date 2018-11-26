55d4841b6ed32fda47fed7825f9d591bd9d6460e
dzikri <dzikrialif99@gmail.com>
2018-11-26 11:57:14 +0700

    class TabelItem untuk setting namaKolom pada tabel

---
+++ src/TabelItem.java
@@ -0,0 +1,25 @@
+/*
+*   DZIKRI ALIF AB
+*   Email : dzikrialif99@gmail.com
+*   Hawlet Packard
+*/
+import javax.swing.table.*;
+public class TabelItem extends DefaultTableModel {
+    
+    // -- menyimpan nama kolom dalam bentuk array
+    private String[] namaKolom;
+
+    // -- konstruktor untuk class TabelItem
+    public TabelItem() {
+        this.namaKolom = new String[]{
+            "Nama","Harga","Jumlah"
+        };
+    }
+    
+    // -- getter untuk namaKolom
+    public String[] getNamaKolom() {
+        return this.namaKolom;
+    }
+    
+    
+}
